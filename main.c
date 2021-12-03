166d2d7de31ee6d21e920780169ec9f6e3a259b7
João Pedro Costa <89428776+joaopcosta-jp@users.noreply.github.com>
2021-12-03 05:29:01 +0000

    funcionando na gambiarra

--- main.c
+++ main.c
@@ -2,31 +2,31 @@
 
 int main(void) {
 
-  int topo[9] = {0};
-  int binario1[8] = {0};
-  int binario2[8] = {0};
-  int res[9] = {0};
+  int topo[10] = {0};
+  int binario1[9] = {0};
+  int binario2[9] = {0};
+  int res[10] = {0};
   int i, x;
 
-  for (i=0;i<=7;i++){
+  for (i=1;i<=8;i++){
 
   printf("Digite o primeiro numero: ");
   scanf("%d", &binario1[i]);
   
   }
 
-  for (i=0;i<=7;i++){
+  for (i=1;i<=8;i++){
 
   printf("Digite o segundo numero: ");
   scanf("%d", &binario2[i]);
   
   }
 
-  printf("Numero digitado: %d%d%d%d%d%d%d%d\n", binario1[0], binario1[1], binario1[2], binario1[3], binario1[4], binario1[5], binario1[6], binario1[7]);
+  printf("Numero digitado: %d%d%d%d%d%d%d%d\n", binario1[1], binario1[2], binario1[3], binario1[4], binario1[5], binario1[6], binario1[7], binario1[8]);
 
-  printf("Numero digitado: %d%d%d%d%d%d%d%d\n", binario2[0], binario2[1], binario2[2], binario2[3], binario2[4], binario2[5], binario2[6], binario2[7]);
+  printf("Numero digitado: %d%d%d%d%d%d%d%d\n", binario2[1], binario2[2], binario2[3], binario2[4], binario2[5], binario2[6], binario2[7], binario2[8]);
 
-   for (x=8, i=7;x>=0 && i>=0;x--, i--){
+   for (x=9, i=8;x>=0 && i>=0;x--, i--){
 
       res[x] = binario1[i] + binario2[i] + topo[x];
 
@@ -51,10 +51,11 @@ int main(void) {
 
     }
   
-  printf("Soma dos números: %d%d%d%d%d%d%d%d%d\n", res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8]);
+  printf("Soma dos números: %d%d%d%d%d%d%d%d%d\n", res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8], res[9]);
 
-  printf("topo: %d%d%d%d%d%d%d%d%d\n", topo[0], topo[1], topo[2], topo[3], topo[4], topo[5], topo[6], topo[7], topo[8]);
-
+/*
+  printf("topo: %d%d%d%d%d%d%d%d%d\n", topo[1], topo[2], topo[3], topo[4], topo[5], topo[6], topo[7], topo[8], topo[9]);
+*/
   return 0;
 
   }
